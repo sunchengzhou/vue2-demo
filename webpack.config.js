0897d4aef299170705fc0681f67af5898a65788b
sunchengzhou <scz20160220@163.com>
2018-02-09 17:39:33 +0800

    webapck-css抽取优化

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path=require('path');
+// 产生或复制html
 const HTMLPlugin=require('html-webpack-plugin');
 const webpack=require('webpack');
+// 抽取css,js文件
+const ExtractPlugin=require('extract-text-webpack-plugin');
+// 清理文件插件
+const cleanWebpackPlugin=require('clean-webpack-plugin')
 // 判断是否为开发环境
 const isDev=process.env.NODE_ENV==="development"
 const config={
     target:'web',
     entry:path.join(__dirname,'src/index.js'),
     output:{
-      filename:'bundle.js',
+      filename:'bundle.[hash:8].js',
       path:path.join(__dirname,'dist')
     },
     module:{
@@ -19,24 +24,6 @@ const config={
         {
           test:/\.jsx$/,
           loader:'babel-loader'
-        },
-        {
-          test:/\.css$/,
-          use:['style-loader','css-loader']
-        },
-        {
-          test:/\.styl/,
-          use:[
-            'style-loader',
-            'css-loader',
-            {
-              loader:"postcss-loader",
-              options:{
-                sourceMap:true
-              }
-            },
-            'stylus-loader'
-          ]
         },
         {
           test:/\.(gif|jpg|jpeg|png|svg)$/,
@@ -63,6 +50,20 @@ const config={
 }
 
 if(isDev){
+  config.module.rules.push({
+      test:/\.styl/,
+      use:[
+        'style-loader',
+        'css-loader',
+        {
+          loader:"postcss-loader",
+          options:{
+            sourceMap:true
+          }
+        },
+        'stylus-loader'
+      ]
+  })
   config.devtool="#cheap-module-eval-source-map"
   config.devServer={
     port:8000,
@@ -76,6 +77,45 @@ if(isDev){
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
+  )
+}else{
+  // 对vue文件单独打包缓存
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename='[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test:/\.styl/,
+      use:ExtractPlugin.extract({
+        fallback:'style-loader',
+        use:[
+          'css-loader',
+          {
+            loader:'postcss-loader',
+            options: {
+              sourceMap: true,
+              minimize: true
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    }
+  )
+  config.plugins.push(
+    // 抽离css，仅抽取公共css
+    new ExtractPlugin('styles.[contentHash:8].css'),
+    // 单独打包vue等公共文件
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
+    }),
+    // 清理插件
+    new cleanWebpackPlugin('./dist/*')
   )
 }
 
